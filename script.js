d8dc60791aaf4c885287b1141c47a012d71ed038
sinisterkyle <70821920+sinisterkyle@users.noreply.github.com>
2020-09-09 23:07:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let fname;
+fname = "Kyle";
+let lname;
+lname = " Selleck";
+let age;
+age = "19";
+
+alert("My name is " + fname + lname);
+alert("I am " + age +" years old");
+alert("It is illegal for me to drink in the states.");
+
+var a = 'I am very excited to learn code in this class. It is super fun so far and I hope I did this right';
+console.log(a);
