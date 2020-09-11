b65867bdf952b86c5bef831514e92d54f217e29c
sinisterkyle <70821920+sinisterkyle@users.noreply.github.com>
2020-09-10 20:02:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ age = "19";
 
 alert("My name is " + fname + lname);
 alert("I am " + age +" years old");
-alert("It is illegal for me to drink in the states.");
+
+if (age < 21) {
+		alert("It is illegal for me to drink in the states.");
+	}  else if (age > 21) {
+		alert("I am legally able to drink in the states.");
+	}  else {
+		alert("I am just barely able to drink legally in the states.");
+	}
 
 var a = 'I am very excited to learn code in this class. It is super fun so far and I hope I did this right';
 console.log(a);
